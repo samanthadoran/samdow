7ffcac2f9f96bec0f43343b6d8b76c2483506f00
Samantha Doran <samanthadoran3@gmail.com>
2016-02-28 21:35:41 -0500

    Add some server commands

--- src/server.nim
+++ src/server.nim
@@ -1,4 +1,6 @@
-import area, user, sets, tables, asyncnet, asyncdispatch, strutils, sequtils
+import area, user
+import sets, tables, asyncnet, asyncdispatch, strutils, sequtils
+
 type
   Server* = ref ServerObj
   ServerObj = object
@@ -16,19 +18,39 @@ proc newServer*(name: string, area: Area): Server =
   result.exits = @[]
   result.socket = newAsyncSocket()
 
+proc processMessage(s: Server, msg: string, u: User) {.async.} =
+  #Determine if a user typed a command
+  let response =
+    case msg.toLower()
+    of "!exits":
+      "Exits: " & $(s.exits.mapIt(it.name).join(", "))
+    of "!info":
+      "Name: " & s.name & "\nArea info: " & s.area.name
+    of "!users":
+      "Users: " & $(s.users.mapIt(it.name).join(", "))
+    else:
+      ""
 
+  if response != "":
+    for u in s.users:
+      await u.socket.send(s.name & ": " & response & "\r\L")
+      echo("Sent message: " & response & " to " & u.name)
 
 proc processUser(s: Server, user: User) {.async.} =
   while true:
     if user.socket.isClosed():
       s.users.excl(user)
       return
+
     let line = await user.socket.recvLine()
-    echo(user.name & " " & line)
+    echo(user.name & ": " & line)
 
     for u in s.users:
       await u.socket.send(user.name & ": " & line & "\r\L")
       echo("Sent message: " & line & " to " & u.name)
+
+    #After sending the users what was said, process it
+    asyncCheck processMessage(s, line, user)
 
 proc processClient(s: Server, c: AsyncSocket) {.async.} =
   while true:
